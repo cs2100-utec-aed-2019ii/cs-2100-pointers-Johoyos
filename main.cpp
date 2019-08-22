8b8e3235b58034e9fcc78c0888ed583a756de990
johoyos <jonathan.hoyos@utec.edu.pe>
2019-08-22 17:43:21 -0500

    tarea hecha

--- main.cpp
+++ main.cpp
@@ -7,4 +7,100 @@ void ejercicio_x (int, int) {
 
 int main (int, char * []){
     return 1;
+    /*
+
+pregunta 1: 
+    1 2 3 4 5 6 7 8 9 10 
+
+pregunta 2:
+    7 5 10
+pregunta 3:
+    T 
+    L
+    T
+pregunta 4:
+    4. *p = 45
+pregunta 5:
+    ERROR, intenta asignar un puntero tipo double,a una variable que almacena u	    tipo char
+
+pregunta 6:
+    void swap(int * x, int*y){
+
+    int temp = *x ;
+	*x = *y
+	*y = temp; 
+
+    }
+
+main(){
+
+ int a = 7 , b =10; 
+ swap(&a, &b);
+ std::cout<<a << " "<<b <<std::endl;
+}
+
+pregunta 7:
+    3  8 
+pregunta 8:
+    0 
+    8
+pregunta 9:
+    8
+    15
+pregunta 10:
+    8
+pregunta 11:
+#define <iostream>   
+int main(){
+    int a= 10;
+    int *ptr = &a; 
+    std::cout<< *ptr <<" "<<&a<<" "<< &ptr <<std::endl;
+    return 0;
+}    
+
+pregunta 12:
+    13  13  13  
+pregunta 13:
+    5 10 7
+pregunta 14:
+    4  1
+
+pregunta 15:
+#define <iostream>
+int main(){
+    int a = 10, b =7 ;
+    int *ptra = &a;
+    int *ptrb = &b;
+    
+    std::cout<< *ptra + *ptrb << std::endl; 
+    
+
+    return 0;
+}
+
+
+pregutna 16:
+
+    19
+
+pregunta 17:
+    Error, esta mal asignado el puntero 
+    
+pregunta 18:
+    18
+
+pregunta 19:
+    elimina el ultimo caracter si es espacio en blanco, o varios espacios en bl	    anco y devuelve lapalabra    
+
+pregunta 20:
+    elimina el caracter de coma(,) de todo el texto y lo devuelve todo el texto	    original sin la ,
+
+
+
+
+
+
+
+
+*/
 }
